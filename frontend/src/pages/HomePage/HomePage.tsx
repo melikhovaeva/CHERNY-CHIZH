9cc467f09bb7d3d446932c9ad023fe254135747c
qalqa-dev <andreybas04@gmail.com>
2026-01-16 20:23:08 +0300

    feat: added Header component and added it to home page component

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,11 @@
+import { Header } from '@/widgets';
 import styles from './HomePage.module.scss';
 
 export function HomePage() {
-  return <div className={styles.main}>HomePage</div>;
+  return (
+    <div className={styles.main}>
+      <Header />
+      HomePage
+    </div>
+  );
 }
