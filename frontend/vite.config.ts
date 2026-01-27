5c9452fb65346d537dc82fa401c7d8fd5048bab0
melikhovaeva <sshultse@vk.com>
2026-01-27 16:34:40 +0300

    fix: warning to unable to gen routeTree fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
+      routesDirectory: './src/app/routes',
     }),
     viteReact(),
   ],
