2407481d7a38cc071f4623e9969e541ee78c6015
melikhovaeva <sshultse@vk.com>
2026-01-30 15:23:36 +0300

    feat: added aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -26,6 +26,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/app/pages', import.meta.url)),
+      '@widgets': fileURLToPath(new URL('./src/app/widgets', import.meta.url)),
+      '@features': fileURLToPath(
+        new URL('./src/app/features', import.meta.url),
+      ),
+      '@entities': fileURLToPath(
+        new URL('./src/app/entities', import.meta.url),
+      ),
+      '@shared': fileURLToPath(new URL('./src/app/shared', import.meta.url)),
+
       '@styles': fileURLToPath(new URL('./src/app/styles', import.meta.url)),
     },
   },
