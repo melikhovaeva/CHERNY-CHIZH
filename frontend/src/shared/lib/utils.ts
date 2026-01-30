82d5fe9d75b08562e05c977636994a0a345bf4c3
melikhovaeva <sshultse@vk.com>
2026-01-30 17:44:44 +0300

    feat: added button component

--- frontend/src/shared/lib/utils.ts
+++ frontend/src/shared/lib/utils.ts
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
 
-const cn = (...classes: string[]) => clsx(...classes);
-
+const cn = (
+  classes: string[],
+  options?: { [key: string]: boolean | undefined },
+) => clsx(...classes, { ...options });
 export { cn };
