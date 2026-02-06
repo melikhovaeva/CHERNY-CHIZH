3287991a114276e5153c553742aa6e898906b325
qalqa-dev <andreybas04@gmail.com>
2026-02-06 21:15:53 +0300

    feat: added Puppies page layout

--- frontend/src/entities/breed/index.ts
+++ frontend/src/entities/breed/index.ts
@@ -1,4 +1,4 @@
 export type { BreedDescription, BreedDescriptionBlock, BreedValue } from './types'
-export { BREED_OPTIONS, getBreedImageUrl } from './config'
+export { BREED_OPTIONS, getBreedFullName, getBreedImageUrl } from './config'
 export type { BreedOption } from './config'
 export { breedDescriptionsMock } from './mock'
