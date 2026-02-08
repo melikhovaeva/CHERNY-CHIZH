490466b9a35295674bc3b4eb7efc8ec346be1be8
qalqa-dev <andreybas04@gmail.com>
2026-02-08 20:21:15 +0300

    feat(BE)(model): added breed and puppies model

--- backend/common/admin.py
+++ backend/common/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from common.models import Breed, BreedDescription, Puppy, PuppyParents
+
+
+class BreedDescriptionInline(admin.StackedInline):
+    model = BreedDescription
+    extra = 0
+    max_num = 1
+    min_num = 1
+    fields = (
+        "appearance",
+        ("character_rating", "character_text"),
+        ("adaptability_rating", "adaptability_text"),
+        ("care_rating", "care_text"),
+        ("activity_rating", "activity_text"),
+    )
+
+class PuppyParentsInline(admin.StackedInline):
+    model = PuppyParents
+    fk_name = "puppy"
+    extra = 0
+    max_num = 1
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ("name", "full_name")
+    inlines = [BreedDescriptionInline]
+
+@admin.register(Puppy)
+class PuppyAdmin(admin.ModelAdmin):
+    list_display = ("name", "international_name", "breed", "status", "birth_date", "sex", "color", "potential")
+    inlines = [PuppyParentsInline]
