9bba197ec76b1ea85e2e83c6debf356df7cd8107
melikhovaeva <sshultse@vk.com>
2026-02-09 14:01:53 +0300

    fix: fixed default route for puppies

--- frontend/src/app/routes/puppies.tsx
+++ frontend/src/app/routes/puppies.tsx
@@ -1,11 +1,10 @@
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
-
-const DEFAULT_BREED_SLUG = 'shpits'
+import { useAppSelector } from '../redux'
 
 export const Route = createFileRoute('/puppies')({
   beforeLoad: ({ location }) => {
     if (location.pathname === '/puppies') {
-      throw redirect({ to: '/puppies/$breedId', params: { breedId: DEFAULT_BREED_SLUG } })
+      throw redirect({ to: '/puppies/$breedId', params: { breedId: useAppSelector((state) => state.selectedBreed.selectedBreedSlug) } })
     }
   },
   component: () => <Outlet />,
