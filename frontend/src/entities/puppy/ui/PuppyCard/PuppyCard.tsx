029d6cba7833d39a66bed58326a3416c56b0fed4
melikhovaeva <sshultse@vk.com>
2026-02-09 19:50:25 +0300

    feat: moved puppy characteristics to PuppyCharacteristic and integrated with documents links

--- frontend/src/entities/puppy/ui/PuppyCard/PuppyCard.tsx
+++ frontend/src/entities/puppy/ui/PuppyCard/PuppyCard.tsx
@@ -4,9 +4,9 @@ import { Button } from '@/shared/ui/components'
 import { useNavigate } from '@tanstack/react-router'
 import type { Puppy } from '../../model/types'
 import {
-  formatPuppyDate,
-  formatPuppyDocuments, getFirstPhotoUrl
+  getFirstPhotoUrl
 } from '../../model/utils'
+import { PuppyCharacteristics } from '../PuppyCharacteristics'
 import styles from './PuppyCard.module.scss'
 
 interface PuppyCardProps {
@@ -42,53 +42,7 @@ export const PuppyCard = ({ puppy, className, detailed = false }: PuppyCardProps
             {puppy.internationalName && <p className={styles.card__subtitle}>{puppy.internationalName}</p>}
           </div>) :
           (<h4 className={styles.card__title}>Характеристики</h4>)}
-        <dl className={styles.card__characteristics}>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Статус:</dt>
-            <dd className={styles.card__characteristicValue}>
-              {puppy.status.label}
-            </dd>
-          </div>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Дата рождения:</dt>
-            <dd className={styles.card__characteristicValue}>
-              {formatPuppyDate(puppy.birthDate)}
-            </dd>
-          </div>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Пол:</dt>
-            <dd className={styles.card__characteristicValue}>
-              {puppy.sex.label}
-            </dd>
-          </div>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Окрас:</dt>
-            <dd className={styles.card__characteristicValue}>{puppy.color}</dd>
-          </div>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Документы:</dt>
-            <dd className={styles.card__characteristicValue}>
-              {formatPuppyDocuments(puppy.documents ?? [])}
-            </dd>
-          </div>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Потенциал:</dt>
-            <dd className={styles.card__characteristicValue}>
-              {puppy.potential?.label ?? 'Не указан'}
-            </dd>
-          </div>
-          <div className={styles.card__characteristic}>
-            <dt className={styles.card__characteristicLabel}>Родители:</dt>
-            <dd className={styles.card__characteristicValue}>
-              {(puppy.parents ?? []).map((parent, index) => (
-                <span key={parent.id}>
-                  {index > 0 && <br />}
-                  {parent.name}
-                </span>
-              ))}
-            </dd>
-          </div>
-        </dl>
+        <PuppyCharacteristics puppy={puppy} />
         <div className={styles.card__buttonContainer}>
           <Button onClick={() => dispatch(openBookingModal())}>Забронировать</Button>
           {!detailed && (
--- frontend/src/entities/puppy/ui/PuppyCharacteristics/PuppyCharacteristics.tsx
+++ frontend/src/entities/puppy/ui/PuppyCharacteristics/PuppyCharacteristics.tsx
@@ -1,7 +1,5 @@
 import type { Puppy } from '../../model/types'
-import {
-  formatPuppyDocuments
-} from '../../model/utils'
+import { formatPuppyDate } from '../../model/utils'
 import styles from './PuppyCharacteristics.module.scss'
 
 interface PuppyCharacteristicsProps {
@@ -9,42 +7,66 @@ interface PuppyCharacteristicsProps {
   className?: string
 }
 
-export const PuppyCharacteristics = ({ puppy, className }: PuppyCharacteristicsProps) => (
-  <dl className={[styles.list, className].filter(Boolean).join(' ')}>
-    <div className={styles.item}>
-      <dt className={styles.label}>Статус:</dt>
-      <dd className={styles.value}>{puppy.status.label}</dd>
-    </div>
-    <div className={styles.item}>
-      <dt className={styles.label}>Дата рождения:</dt>
-      <dd className={styles.value}>{puppy.birthDate}</dd>
-    </div>
-    <div className={styles.item}>
-      <dt className={styles.label}>Пол:</dt>
-      <dd className={styles.value}>{puppy.sex.label}</dd>
-    </div>
-    <div className={styles.item}>
-      <dt className={styles.label}>Окрас:</dt>
-      <dd className={styles.value}>{puppy.color}</dd>
-    </div>
-    <div className={styles.item}>
-      <dt className={styles.label}>Документы:</dt>
-      <dd className={styles.value}>{formatPuppyDocuments(puppy.documents)}</dd>
-    </div>
-    <div className={styles.item}>
-      <dt className={styles.label}>Потенциал:</dt>
-      <dd className={styles.value}>{puppy.potential?.label ?? 'Не указан'}</dd>
-    </div>
-    <div className={styles.item}>
-      <dt className={styles.label}>Родители:</dt>
-      <dd className={styles.value}>
-        {puppy.parents?.map((parent, index) => (
-          <span key={parent.id}>
-            {index > 0 && <br />}
-            {parent.name}
-          </span>
-        ))}
-      </dd>
-    </div>
-  </dl>
-)
+export const PuppyCharacteristics = ({ puppy, className }: PuppyCharacteristicsProps) => {
+
+  const documentsExists = puppy.documents && puppy.documents?.length > 0
+
+  return (
+    <dl className={[styles.list, className].filter(Boolean).join(' ')}>
+      <div className={styles.item}>
+        <dt className={styles.label}>Статус:</dt>
+        <dd className={styles.value}>{puppy.status.label}</dd>
+      </div>
+      <div className={styles.item}>
+        <dt className={styles.label}>Дата рождения:</dt>
+        <dd className={styles.value}>{formatPuppyDate(puppy.birthDate)}</dd>
+      </div>
+      <div className={styles.item}>
+        <dt className={styles.label}>Пол:</dt>
+        <dd className={styles.value}>{puppy.sex.label}</dd>
+      </div>
+      <div className={styles.item}>
+        <dt className={styles.label}>Окрас:</dt>
+        <dd className={styles.value}>{puppy.color}</dd>
+      </div>
+      {documentsExists && (
+        <div className={styles.item}>
+          <dt className={styles.label}>Документы:</dt>
+          <dd className={styles.value}>
+            {
+              puppy.documents?.map((document) => (
+
+                document.url ? (
+                  <a key={document.id}
+                    className={styles.value__link}
+                    href={document.url}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    {document.name}
+                  </a>
+                ) : (
+                  <span key={document.id}>{document.name}</span>
+                )
+              ))
+            }
+          </dd>
+        </div>
+      )}
+      <div className={styles.item}>
+        <dt className={styles.label}>Потенциал:</dt>
+        <dd className={styles.value}>{puppy.potential.label}</dd>
+      </div>
+      <div className={styles.item}>
+        <dt className={styles.label}>Родители:</dt>
+        <dd className={styles.value}>
+          {puppy.parents?.map((parent, index) => (
+            <span key={parent.id}>
+              {index > 0 && <br />}
+              {parent.name}
+            </span>
+          ))}
+        </dd>
+      </div>
+    </dl >
+  )
+}
