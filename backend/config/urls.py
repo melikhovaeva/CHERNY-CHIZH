bbed8e957d049c5fccd7e7b3336f7455179b3532
melikhovaeva <sshultse@vk.com>
2026-02-24 16:34:17 +0300

    feat: added api versioning and FE integration

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path("api/", include("api_gateway.urls")),
+    path("api/v1/", include("api_gateway.urls")),
 ]
