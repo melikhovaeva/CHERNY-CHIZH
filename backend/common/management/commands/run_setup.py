540ca889ce971a5140d10e0f6c22b172f08f556e
qalqa-dev <andreybas04@gmail.com>
2026-02-24 23:39:01 +0300

    feat(BE): added check for filling commands is db empty

--- backend/common/management/commands/run_setup.py
+++ backend/common/management/commands/run_setup.py
@@ -1,15 +1,25 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
+
 class Command(BaseCommand):
-    help = 'Тестовая команда для наполнения бд'
+    help = "Тестовая команда для наполнения БД"
 
     def handle(self, *args, **options):
-        call_command('ensure_minio_bucket')
-        call_command('migrate')
-        call_command('run_all_static_commands')
-        call_command('run_all_test_commands')
+        call_command("ensure_minio_bucket")
+        call_command("migrate")
+        from common.models import Dog 
         from django.contrib.auth import get_user_model
+
+        if not Dog.objects.exists():
+            call_command("run_all_static_commands")
+            call_command("run_all_test_commands")
+        else:
+            self.stdout.write(
+                self.style.WARNING(
+                    "БД не пуста, пропускаю заполнение тестовыми и статическими данными."
+                )
+            )
 
         User = get_user_model()
         if not User.objects.filter(email="admin@admin.com").exists():
@@ -17,5 +27,5 @@ class Command(BaseCommand):
                 email="admin@admin.com",
                 password="admin",
                 first_name="admin",
-                last_name="admin"
+                last_name="admin",
             )
