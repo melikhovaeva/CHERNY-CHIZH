2aabe48f0bec07b5fc78a27bf41fa11252b7dec5
qalqa-dev <andreybas04@gmail.com>
2026-02-26 01:49:34 +0300

    BREAKING-CHANGES(FE): fully reworked architecture
    - Reworked main fe archetecture to packages(lib) one
    - Added space to another app (admin)
    - Prepared to integration with openapi contract with BE to remove
    unmatches in FE models

---
+++ frontend/packages/admin-app/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { App } from './App';
+
+const rootElement = document.getElementById('app');
+
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
