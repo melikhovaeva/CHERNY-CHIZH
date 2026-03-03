15f304083b9d19b66c07384305fbad69de652c83
melikhovaeva <sshultse@vk.com>
2026-03-03 20:03:40 +0300

    feat(FE): added tanstack loaders to all pages with initial loaded data

--- frontend/packages/user-app/src/app/routes/dogs.$breedId.$dogId.tsx
+++ frontend/packages/user-app/src/app/routes/dogs.$breedId.$dogId.tsx
@@ -1,6 +1,27 @@
 import { DogDetailsPage } from '@/pages'
 import { createFileRoute } from '@tanstack/react-router'
+import { store } from '../redux'
+import { puppyApi } from '@/entities/puppy/api/puppy.api'
 
 export const Route = createFileRoute('/dogs/$breedId/$dogId')({
+  loader: async ({ params: { dogId } }) => {
+    const id = Number(dogId)
+
+    if (Number.isNaN(id)) {
+      return null
+    }
+
+    const queryPromise = store.dispatch(
+      puppyApi.endpoints.getPuppy.initiate(id),
+    )
+
+    try {
+      await queryPromise
+    } finally {
+      queryPromise.unsubscribe()
+    }
+
+    return null
+  },
   component: DogDetailsPage,
 })
--- frontend/packages/user-app/src/app/routes/dogs.$breedId.index.tsx
+++ frontend/packages/user-app/src/app/routes/dogs.$breedId.index.tsx
@@ -1,6 +1,31 @@
 import { DogsPage } from '@/pages'
 import { createFileRoute } from '@tanstack/react-router'
+import { store, setSelectedBreed } from '../redux'
+import { breedApi } from '@/entities/breed/api/breed.api'
+import { dogApi } from '@/entities/puppy/api/dog.api'
 
 export const Route = createFileRoute('/dogs/$breedId/')({
+  loader: async ({ params: { breedId } }) => {
+    const breedsPromise = store.dispatch(
+      breedApi.endpoints.getBreeds.initiate(),
+    )
+    const dogsPromise = store.dispatch(
+      dogApi.endpoints.getDogsByBreed.initiate({
+        breedSlug: breedId,
+        page: 1,
+      }),
+    )
+
+    store.dispatch(setSelectedBreed(breedId))
+
+    try {
+      await Promise.all([breedsPromise, dogsPromise])
+    } finally {
+      breedsPromise.unsubscribe()
+      dogsPromise.unsubscribe()
+    }
+
+    return null
+  },
   component: DogsPage,
 })
